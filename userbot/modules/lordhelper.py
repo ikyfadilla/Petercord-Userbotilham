27eaa3ce4edd7f545adaa2ad74643e59b30bc267
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-03-11 22:13:40 +0700

    Update lordhelper.py

--- userbot/modules/lordhelper.py
+++ userbot/modules/lordhelper.py
@@ -8,26 +8,26 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.lordhelp$")
+@register(outgoing=True, pattern="^.petercordhelp$")
 async def usit(e):
     await e.edit(
-        f"**Hai Lord {DEFAULTUSER} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/liualvinas)"
-        "\n[Repo](https://github.com/zora24/Lord-Userbot)"
-        "\n[Instagram](Instagram.com/liualvinas_)")
+        f"**Hai Petercord {DEFAULTUSER} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+        "\n[Telegram](t.me/bismillahselaluadaa)"
+        "\n[Repo](https://github.com/ilham77mansiz/Petercord-Userbotilham)"
+        "\n[Instagram](Instagram.com/imansiezz_)")
 
 
-@register(outgoing=True, pattern="^.lordvar$")
+@register(outgoing=True, pattern="^.petercordvar$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Zora24/Lord-Userbot/Lord-Userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/ilham77mansiz/Lord-Userbot/Lord-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
-    "lordhelper":
-    "`.lordhelp`\
-\nUsage: Bantuan Untuk Lord-Userbot.\
-\n`.lordvar`\
+    "petercordhelper":
+    "`.petercordhelp`\
+\nUsage: Bantuan Untuk Petercord-Userbot.\
+\n`.petercordvar`\
 \nUsage: Melihat Daftar Vars."
 })
