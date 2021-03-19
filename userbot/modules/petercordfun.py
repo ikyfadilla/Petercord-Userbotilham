70054c168ffa6765596d2c8d5d275c71fdb6a106
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-03-19 15:56:16 +0700

    Update petercordfun.py

--- userbot/modules/petercordfun.py
+++ userbot/modules/petercordfun.py
@@ -47,6 +47,18 @@ async def _(event):
     await tap[0].click(event.chat_id)
     await event.delete()
 
+@register(outgoing=True, pattern=r"^\.quiz(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    botusername = "@QuizBot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
 # Ported For Petercord-Userbot By bismillahselaluadaa/ILham
 
 CMD_HELP.update({
